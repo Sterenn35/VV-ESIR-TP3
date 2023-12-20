e5a7b36916b773cac17081956d4f1df6805be964
Sterenn35 <slh35@hotmail.fr>
2023-12-20 21:56:10 +0100

    Add tests

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -12,7 +12,7 @@ class Date implements Comparable<Date> {
     private int year;
     public Date(int day, int month, int year) {
         if (!isValidDate(day,month,year)) {
-            throw new BadDateException("La date n'est pas valide");
+           throw new BadDateException("La date n'est pas valide");
         } else {
             this.day = day;
             this.month = month;
@@ -23,26 +23,18 @@ class Date implements Comparable<Date> {
     public static boolean isValidDate(int day, int month, int year) {
         // variables for lisibility
         boolean badYear = year <= 0;
-        boolean badMonth = month < 0 || month > 12;
-        boolean badDay = day < 0 || day > 31;
+        boolean badMonth = month <= 0 || month > 12;
+        boolean badDay = day <= 0 || day > 31;
         if (badYear || badMonth || badDay) return false;
-        System.out.println("Not that bad");
         boolean month30 = month == 4 || month == 6 || month == 9 || month == 11; // months which have 30 days
         if (month30 && day > 30) return false;
-        System.out.println("No exceed in days");
         boolean badFebruaryDate = month == 2 && ((isLeapYear(year) && day > 29) || (!isLeapYear(year) && day > 28));
-        if (badFebruaryDate) {
-            System.out.println("Problem in february");
-            return false;
-        } else {
-            System.out.println("No problem");
-            return true;
-        }
+        return !badFebruaryDate;
     }
 
     public static boolean isLeapYear(int year) {
         // une année bissextile est divisible par 4 mais pas par 100 sauf si elle est aussi divisible par 400
-        return year%4 == 0 && (year%100 !=0 || year%100 == 0 && year%400 == 0);
+        return year % 400 == 0 || ( year % 4 == 0 && year % 100 != 0 );
     }
 
     public Date nextDate() {
@@ -52,7 +44,7 @@ class Date implements Comparable<Date> {
         boolean month30 = month == 4 || month == 6 || month == 9 || month == 11; // months which have 30 day
         // Cas pour lequel on démarre un nouveau mois
         boolean leapYear = isLeapYear(year);
-        boolean endOfMonth = (month30 && day == 30) || (month == 2 && ((leapYear && day == 29) || (leapYear && day == 28)));
+        boolean endOfMonth = (month30 && day == 30) || (day == 31 && month != 12) || (month == 2 && ((leapYear && day == 29) || (!leapYear && day == 28)));
         if (endOfMonth)  {
             nextDay = 1;
             nextMonth = this.month + 1;
@@ -66,8 +58,8 @@ class Date implements Comparable<Date> {
                 nextYear = year + 1;
             } else { // Cas lambda
                 nextDay = day + 1;
-                nextMonth = month + 1;
-                nextYear = year + 1;
+                nextMonth = month;
+                nextYear = year;
             }
         }
         return new Date(nextDay, nextMonth, nextYear);
@@ -120,24 +112,11 @@ class Date implements Comparable<Date> {
             if (this.month > other.month) return -1;
             // Si les mois sont égaux
             if (this.day < other.day) return 1;
-            if (this.month > other.day) return -1;
+            if (this.day > other.day) return -1;
             return 0; // les dates sont identiques
         } else { // Si other est null, on renvoie une exception
             throw new NullPointerException("La date fournie est null");
         }
     }
-
-    public int getDay() {
-        return day;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
 
 }
